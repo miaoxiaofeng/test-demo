f18b6679abf4248d577ace9a6407ffd255aefd93
miaoxiaofeng <miaoxiaofeng666@163.com>
2019-08-30 11:44:37 +0800

    新增demo01

---
+++ src/main/java/demo01.java
@@ -0,0 +1,9 @@
+/**
+ * @author miaoxiaofeng
+ * @create 2019-08-30 11:43
+ */
+public class demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
